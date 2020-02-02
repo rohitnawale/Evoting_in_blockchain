97bb89e37e8ff66191ffb3bc1077cca56e463ad1
Rohit Nawale <40070862+rohitnawale@users.noreply.github.com>
2020-02-02 16:49:50 +0530

    Added home page

--- mysite/voting/urls.py
+++ mysite/voting/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('instructions/', views.instructions, name='instructions'),
     path('castVote/', views.cast_vote, name='castVote'),
     path('done/', views.done, name='done'),
+    path('home/', views.home, name='home'),
 ]
